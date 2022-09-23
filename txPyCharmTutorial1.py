23d6357ccb0b7813b69f4cfd6224e14f55d1ab8e
tomanix <tomanix@openup.pt>
2022-09-23 14:56:14 +0100

    mais um commit 2

--- txPyCharmTutorial1.py
+++ txPyCharmTutorial1.py
@@ -9,9 +9,9 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script.:wq
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print_hi('tomanix')
+print_hi('tomanix14')
